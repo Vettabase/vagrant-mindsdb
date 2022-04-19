96d5250eb04d8700d78ba93b8cd4233271ce9d74
Federico Razzoli <federico.razzoli@vettabase.com>
2022-04-19 10:31:04 +0100

    Add a bootstrap.sh draft

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
 
-# do nothing
+# THIS IS A DRAFT.
+# IT IS ABSOLUTELY NOT READY TO BE USED BY THE PUBLIC.
+# Play with it as you please, and feel free to contribute.
+
+
+apt-get update -y
+apt-get install -y \
+    python3 \
+    python3-pip
+
+apt install -y python3.8-venv
+python3 -m venv mindsdb
+source mindsdb/bin/activate
+
+pip install --upgrade pip
+pip install mindsdb
+pip freeze
+
+apt-get install -y \
+    mariadb-client
+
+python -m mindsdb --config=$CONFIG_FILE_PATH --api=$APIs &
+
+
+
+# NOTES
+
+# The Docker image places the config file here:
+# /root/mindsdb_config.json
+
+# http,mysql,mongodb
+
+# to connect:
+# mysql -h127.0.0.1 -P47335 -umindsdb
